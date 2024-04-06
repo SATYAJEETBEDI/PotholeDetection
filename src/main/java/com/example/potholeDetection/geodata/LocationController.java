246005e32f912ed9c3cabff103da7bcfef66c690
Satyajeet Bedi <satyajeetbedi145@gmail.com>
2024-04-07 03:49:45 +0530

    clustering added

--- src/main/java/com/example/potholeDetection/geodata/LocationController.java
+++ src/main/java/com/example/potholeDetection/geodata/LocationController.java
@@ -3,6 +3,9 @@ package com.example.potholeDetection.geodata;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,20 +18,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class LocationController {
 
     // @Autowired
-    private final LocationRepository locationRepository;
+    private final LocationService locationService;
 
-    public LocationController(LocationRepository locationRepository) {
-        this.locationRepository = locationRepository;
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
     }
+
+    @PostMapping("/live")
+    public Optional<String> alert(@RequestBody Location location) {
+        
+        return locationService.googleDistance(location);
+    }
+    
+
 
     @PostMapping("/")
     void create(@RequestBody Location location) {
-        locationRepository.save(location);
+        locationService.create(location);
     }
     
-    @DeleteMapping("/")
-    void delete(@RequestBody Location location) {
-        locationRepository.delete(locationRepository.findByLatitudeAndLongitude(location.getLatitude(), location.getLongitude()));
-    }
     
 }
---
+++ src/main/java/com/example/potholeDetection/geodata/LocationService.java
@@ -0,0 +1,50 @@
+package com.example.potholeDetection.geodata;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.potholeDetection.clustering.Centroid;
+import com.example.potholeDetection.clustering.CentroidRepository;
+import com.example.potholeDetection.clustering.KMeansClustering;
+
+@Service
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+    private final KMeansClustering kMeansClustering;
+    private final CentroidRepository centroidRepository;
+    
+    private final int k = 2;
+
+
+    public LocationService(LocationRepository locationRepository, KMeansClustering kMeansClustering, CentroidRepository centroidRepository) {
+        this.locationRepository = locationRepository;
+        this.kMeansClustering = kMeansClustering;
+        this.centroidRepository = centroidRepository;
+    }
+
+    public Optional<String> googleDistance(Location location) {
+        //implementation of google distance matrix api pending
+        return Optional.of("SOMETHING");
+    }
+
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
+
+    public void create(Location location) {
+
+        //checking for duplicate according to distance is pending
+        //if(locationRepository.findByLatitudeAndLongitude(location.getLatitude(), location.getLongitude())==null){
+            
+        locationRepository.save(location);
+        List<Centroid> centroids=kMeansClustering.implement(getAllLocations(), k);
+
+        // for(Centroid centroid:centroids){
+        //     centroidRepository.save(centroid);
+        //}
+    }
+
+}
